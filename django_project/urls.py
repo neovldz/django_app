02cda30cf7c172c2d4a6174f449cbff9c50a45f8
Noe Valdez <neovldz@gmail.com>
2015-08-07 10:47:04 -0500

    urls changed

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,7 +6,7 @@ from football import urls as football_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^soccer-api', include(soccer_urls)),
-    url(r'^football-api', include(football_urls)),
+    url(r'^soccer-api/', include(soccer_urls)),
+    url(r'^football-api/', include(football_urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))  # noqa
 ]
