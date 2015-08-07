a0a3e9356f8f18965cd845492e641826901e2075
Noe Valdez <neovldz@gmail.com>
2015-08-06 19:32:56 -0500

    fixed urls

---
+++ football/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+
+from rest_framework import routers
+
+from football import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'players', views.PlayerViewSet)
+router.register(r'teams', views.TeamViewSet)
+router.register(r'coaches', views.CoachViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
